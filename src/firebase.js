2f067666ce870fb254573aa9c3ebe483328df312
Manish-choubey <109017549+Manish-choubey@users.noreply.github.com>
2022-12-18 13:20:58 +0530

    chat

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: "chat-ab746.firebaseapp.com",
-  projectId: "chat-ab746",
-  storageBucket: "chat-ab746.appspot.com",
-  messagingSenderId: "901216368405",
-  appId: "1:901216368405:web:8ec942ee51611df5c49b1c",
+  apiKey: "AIzaSyAmLh53wv34oJLdbQ6gv_e5xqVDCcvOFHY",
+  authDomain: "chat-bd1ff.firebaseapp.com",
+  projectId: "chat-bd1ff",
+  storageBucket: "chat-bd1ff.appspot.com",
+  messagingSenderId: "874012594020",
+  appId: "1:874012594020:web:202f7ec483025fd38a7471"
 };
 
 // Initialize Firebase
